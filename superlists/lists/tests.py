1892fd65141f1bc6516159758c1ebd586be62881
Thadeu José <thadeujs@gmail.com>
2021-04-17 22:22:59 -0300

    Add one item in the list
    Add one list in the list. Don't add more then one yet

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -14,3 +14,14 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
+
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        item_text = 'A new list item'
+        data = {'item_text': item_text}
+        response = self.client.post('/', data)
+        self.assertIn(item_text, response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
